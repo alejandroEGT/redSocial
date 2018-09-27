2b9df6753f3fc2ca3b3d8d4c7c4a5a11bcdd6b3c
alejandroEGT <alejandro.godoy10@inacapmail.cl>
2018-09-27 16:17:35 -0300

    nada2

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         //$this->call(UsersTableSeeder::class);
-        $this->call(EstadoSolicitudTablaSeeder::class);
+       // $this->call(EstadoSolicitudTablaSeeder::class);
         $this->call(SexoTablaSeeder::class);
     }
 }
