b9090ba313915309f063d83afd1bb04c570869a2
alejandroEGT <alejandro.godoy10@inacapmail.cl>
2020-07-09 19:19:48 -0400

    validate email

--- resources/assets/js/components/Home/Home.vue
+++ resources/assets/js/components/Home/Home.vue
@@ -2,11 +2,11 @@
   <div>
     <b-nav style="background: rgb(176,58,46);
 background: linear-gradient(90deg, rgba(176,58,46,1) 0%, rgba(155,89,182,1) 31%, rgba(155,89,182,1) 31%, rgba(236,112,99,1) 86%, rgba(231,76,60,1) 99%);" small>
-        <b-nav-item active @click="ruta('index')">Inicio</b-nav-item>
+        <b-nav-item active @click="ruta('pm_index')">Inicio</b-nav-item>
         <b-nav-item v-if="this.$auth.check()" @click="ruta('profile')">Mi perfil</b-nav-item>
         <b-nav-item>Link</b-nav-item>
         <b-nav-item>Another Link</b-nav-item>
-        <b-nav-item disabled>Disabled</b-nav-item>
+        <b-nav-item @click="logout">Salir</b-nav-item>
       </b-nav>
 
        <div style="background:#F4F6F6">
